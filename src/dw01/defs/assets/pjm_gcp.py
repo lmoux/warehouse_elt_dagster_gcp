1c20cc7755afeabf981255106b770bf79b4aabf2
Luis Moux-Dominguez <LuisMouxDominguez@gmail.com>
2025-06-11 19:37:30 -0400

    feat: process all

--- src/dw01/defs/assets/pjm_gcp.py
+++ src/dw01/defs/assets/pjm_gcp.py
@@ -8,7 +8,7 @@ from dagster_gcp.gcs import GCSResource
 from google.cloud import bigquery as bigqueryLib
 
 import src.dw01.pjm
-from src.dw01.pjm import PjmFtrScheduleFile, PjmFtrModelUpdateFile
+from src.dw01.pjm import PjmFtrScheduleFile, PjmFtrModelUpdateFile, PjmFileKind
 from src.dw01.utils import download_file_locally
 
 # sys.path.append("../../../")
@@ -19,6 +19,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
 )
 # TODO: turn into configuration parameters
 big_query_dataset_id = "electricity-dw01.pjm_dataset"
+gcs_bucket_id = "dw01_bucket"
 
 
 @dg.asset(
@@ -34,18 +35,36 @@ def file_uploader_to_gcp(
     # Apparently this client doesn't need a `with` (IDisposable `using`)
     gcs_client = gcs.get_client()
 
-    bucket, blobs = retrieve_bucket_and_blobs(gcs_client, "dw01_bucket")
-
+    log.info(f"Connecting & retrieving files in GCP Storage: gs://{gcs_bucket_id}")
+    bucket, blobs = retrieve_bucket_and_blobs(gcs_client, gcs_bucket_id)
     uploaded_files = set()
+    ftr_url = context.op_config["ftr_url"]
+    log.info(f"Identifying all sought files from {ftr_url}")
+    files_to_download = src.dw01.pjm.get_urls_all_ftr(ftr_url)
+    log.info(f"There were a total of {len(files_to_download)} prospect sought files")
 
-    for next_file in src.dw01.pjm.get_urls_all_ftr(context.op_config["ftr_url"]):
+    indexes_to_remove = []
+    for ix in range(0, len(files_to_download)):
+        # at the top level, we have raw files which have distinct, canonical names
+        f = files_to_download[ix]
+        if f.nice_filename is None or f.nice_filename in blobs:
+            indexes_to_remove.append(ix)
+
+    for ix in reversed(indexes_to_remove):
+        files_to_download.pop(ix)
+
+    log.info(
+        f"There were a total of {len(files_to_download)} actual sought files we'll retrieve and upload"
+    )
+
+    for next_file in files_to_download:
         if next_file is None:
             continue
         blob = bucket.blob(next_file.nice_filename)
 
-        if blob.exists():
-            # this seems inefficient; too many round trips, perhaps try to query for the X most recent files
-            continue
+        # if blob.exists():
+        #     # this seems inefficient; too many round trips, perhaps try to query for the X most recent files
+        #     continue
 
         log.info(f"Downloading file {next_file.nice_filename}")
 
@@ -55,6 +74,7 @@ def file_uploader_to_gcp(
             next_file.nice_filename,
             overwrite=False,
         )
+        log.info(f"Uploading file {next_file.nice_filename} to GCS")
         blob.upload_from_filename(local_file)
         uploaded_files.add(local_file)
     return dg.MaterializeResult(
@@ -84,7 +104,7 @@ pjm_table_schemas = {
         bigqueryLib.SchemaField("market_name", "STRING", mode="REQUIRED"),
         bigqueryLib.SchemaField("product", "STRING", mode="REQUIRED"),
         bigqueryLib.SchemaField("period", "STRING", mode="REQUIRED"),
-        bigqueryLib.SchemaField("auction_round", "INTEGER", mode="REQUIRED"),
+        bigqueryLib.SchemaField("auction_round", "string", mode="REQUIRED"),
         bigqueryLib.SchemaField("bidding_opening", "DATETIME", mode="REQUIRED"),
         bigqueryLib.SchemaField("bidding_closing", "DATETIME", mode="REQUIRED"),
         bigqueryLib.SchemaField("results_posted", "DATETIME", mode="REQUIRED"),
@@ -120,7 +140,8 @@ def gcp_file_processor(
     log = context.log
 
     gcs_client = gcs.get_client()
-    bucket, blobs = retrieve_bucket_and_blobs(gcs_client, "dw01_bucket")
+    log.info(f"Connecting & retrieving files in GCP Storage: gs://{gcs_bucket_id}")
+    bucket, blobs = retrieve_bucket_and_blobs(gcs_client, gcs_bucket_id)
 
     processed_files = []
 
@@ -136,6 +157,9 @@ def gcp_file_processor(
         # for table in tables:
         #     print("{}.{}.{}".format(table.project, table.dataset_id, table.table_id))
 
+        log.info(
+            f"Connecting & retrieving tables in BigQuery: bq://{big_query_dataset_id}"
+        )
         tables_gcp = set(
             table.table_id for table in bg_client.list_tables(big_query_dataset_id)
         )
@@ -144,12 +168,19 @@ def gcp_file_processor(
             t for t in pjm_table_schemas.keys() if t not in tables_gcp
         )
 
+        if len(missing_table_names) > 0:
+            log.info(
+                f"Identified missing tables in BigQuery: {','.join(missing_table_names)}"
+            )
+
         for missing_table_name in missing_table_names:
+            full_table_name = f"{big_query_dataset_id}.{missing_table_name}"
             big_table = bigqueryLib.Table(
-                f"{big_query_dataset_id}.{missing_table_name}",
+                full_table_name,
                 schema=pjm_table_schemas[missing_table_name],
             )
             bg_client.create_table(table=big_table)
+            log.info(f"Created BigQuery table: {full_table_name}")
 
         should_merge_model_changes = False
         should_merge_auction_calendars = False
@@ -164,29 +195,35 @@ def gcp_file_processor(
             #   df = pd.read_csv(io.StringIO(data))
             # attempt2: maybe use gcsfs
 
-            if "ftr-arr-market-schedule" in blob_name:
-                log.info(f"Downloading PjmFtrScheduleFile: {blob_name}")
-                # data = io.BytesIO(blob.download_as_bytes())
-                blob.download_to_filename(blob_name)
-                log.info(f"Attempt at parsing PjmFtrScheduleFile: {blob_name}")
-                parsed_result = PjmFtrScheduleFile(blob_name)
-                # merge into the auction_schedule table
-                new_name = blob_name.split(".")[0] + ".csv"
+            local_blob_name = os.path.join("downloads/", blob_name)
+            new_name = os.path.basename(blob_name.split(".")[0] + ".csv")
+            cleaned_file = bucket.blob("cleaned/" + new_name)
+            if cleaned_file.exists():
+                continue
 
-                cleaned_file = bucket.blob("cleaned/" + new_name)
-                if cleaned_file.exists():
-                    continue
+            kind = PjmFileKind.FtrSchedule
+
+            if not os.path.exists(local_blob_name):
+                log.info(f"Downloading file: {blob_name}")
+                blob.download_to_filename(local_blob_name)
+
+            if "ftr-arr-market-schedule" in blob_name:
+                kind = PjmFileKind.FtrSchedule
+                log.info(f"Attempt at parsing PjmFtrScheduleFile: {blob_name}")
+                parsed_result = PjmFtrScheduleFile(local_blob_name)
+                # merge into the auction_schedule table
 
                 cleaned_file.upload_from_string(
                     pd.DataFrame(parsed_result.auction_data_frame).to_csv(index=False),
                     "text/csv",
                 )
-                print(f"Merge new PjmFtrScheduleFile: {blob_name}")
+                log.info(f"Merge new PjmFtrScheduleFile: {blob_name}")
                 processed_files.append(blob_name)
                 should_merge_auction_calendars = True
             elif blob_name.startswith("ftr-model-update") and blob_name.endswith(
                 ".csv"
             ):
+                kind = PjmFileKind.FtrSchedule
                 cleaned_file = bucket.blob("cleaned/" + blob_name)
                 if cleaned_file.exists():
                     continue
@@ -198,7 +235,7 @@ def gcp_file_processor(
                 cleaned_file.upload_from_string(
                     parsed_result.changes.to_csv(index=False), "text/csv"
                 )
-                print(f"Merge new PjmFtrModelUpdateFile: {blob_name}")
+                log.info(f"Merge new PjmFtrModelUpdateFile: {blob_name}")
                 processed_files.append(blob_name)
                 should_merge_model_changes = True
 
@@ -217,6 +254,27 @@ def gcp_file_processor(
                       from_id,from_txt_zone,from_substation,from_voltage,from_equipment,from_name,
                       to_id,to_txt_zone,to_substation,to_voltage, to_equipment, to_name);
             """)
+            log.info("Synced the main modeled updates table")
+
+        if should_merge_auction_calendars:
+            bg_client.query("""
+                merge `pjm_dataset.ftr_auction_calendar_events` as t
+                using pjm_dataset.ftr_auction_calendar_events_from_files as s
+                on t.version = s.version and t.market_name = s.market_name
+                    and t.product = s.product
+                    and t.auction_round = s.auction_round
+                    and t.period = s.period
+                when not matched by target then
+                  insert (
+                      version, market_name, product, period, auction_round,
+                      bidding_opening, bidding_closing, results_posted,
+                      contract_start, contract_end)
+                  values (
+                      version, market_name, product, period, auction_round,
+                      bidding_opening, bidding_closing, results_posted,
+                      contract_start, contract_end);
+            """)
+            log.info("Synced the main auction calendar events table")
 
     return dg.MaterializeResult(
         metadata={
